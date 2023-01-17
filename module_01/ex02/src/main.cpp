ba2f495bc269c422c804735970fdcd1293c45d80
Daniexp <dani00exp@gmail.com>
2023-01-17 23:08:01 +0100

    exercise 02 finish

--- module_01/ex02/src/main.cpp
+++ module_01/ex02/src/main.cpp
@@ -3,5 +3,10 @@
 
 int	main(void)
 {
-	std::cout << "The program compiled sucessfully." << std::endl;
+	std::string	strEj="HI THIS IS BRAIN";
+	std::string*	stringPTR = &strEj;
+	std::string&	stringREF = strEj;
+
+	std::cout << "La dirección de memoria de la variable de tipo string: " << &strEj << std::endl << "La dirección de memoria contenida en stringPTR: " << stringPTR << std::endl <<  "La dirección de memoria contenida en stringREF: " << &stringREF << std::endl;
+	std::cout << "El valor de la varible string: " << strEj << std::endl << "El valor apuntado por stringPTR: " << *stringPTR << std::endl << "El valor apuntado por stringREF: " << stringREF << std::endl;
 }
