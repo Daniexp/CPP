147fc809770b2b288721efa1c41d3988793ca399
Daniel Exposito lopez <dexposit@c1r16s4.42madrid.com>
2023-07-18 18:05:22 +0200

    repare empty include for module06 ex00

--- module_00/ex01/inc/Contact.hpp
+++ module_00/ex01/inc/Contact.hpp
@@ -5,7 +5,7 @@ class Contact
 	std::string	firstname;
 	std::string	lastname;
 	std::string	nickname;
-	std::string phonenumber;
+	std::string	phonenumber;
 	std::string	darkestsecret;
 	public:
 		Contact(std::string fst_nm, std::string lst_nm, std::string nick, std::string phn, std::string dks);
--- module_06/ex00/src/main.cpp
+++ module_06/ex00/src/main.cpp
@@ -1,4 +1,6 @@
-#include <.hpp>
+//#include <ex00.hpp>
+#include <iostream>
+#include <string>
 int main(void)
 {
 	std::cout << "The proyect was created sucessfully." << std::endl;
