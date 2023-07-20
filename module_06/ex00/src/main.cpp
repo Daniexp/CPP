60c9ad4ad1ee83a7913bb5013715a553d07b1e92
Daniel Exposito lopez <dexposit@c1r16s2.42madrid.com>
2023-07-20 19:54:52 +0200

    try to use doctest, but is broken should separate and create ttheir own makefile

--- module_06/ex00/src/main.cpp
+++ module_06/ex00/src/main.cpp
@@ -1,6 +1,8 @@
-//#include <ex00.hpp>
 #include <iostream>
 #include <string>
+//#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+//int main(int argc, char **argv)
 int main(void)
 {
 	std::cout << "The proyect was created sucessfully." << std::endl;
