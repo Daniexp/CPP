b42e1741da2787aa2ec85219662cc997ba2cb3dd
Daniel Exposito lopez <dexposit@c3r1s6.42madrid.com>
2024-01-04 20:02:23 +0100

    add basic structure for ex01

---
+++ module_07/ex01/test/ex01_test.cpp
@@ -0,0 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <iostream>
+#include <sstream>
+#include <ex01.hpp>
+
+TEST_CASE("Example Test_Case")
+{
+	MESSAGE("Example test case is executed successfully...");
+}
