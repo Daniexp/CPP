37102afac20a951835ab1c731e7d0a163c125285
Daniel Exposito lopez <dexposit@c2r5s3.42madrid.com>
2024-04-16 20:29:14 +0200

    add 3 test, int valid, int invalid, char valid

--- module_08/ex00/test/ex00_test.cpp
+++ module_08/ex00/test/ex00_test.cpp
@@ -2,8 +2,49 @@
 #include "doctest.h"
 #include <iostream>
 #include <sstream>
+#include <easyfind.hpp>
 
-TEST_CASE("Example Test_Case")
+TEST_CASE("easyfind - valid: search int on a int vtr")
 {
-	MESSAGE("Example test case is executed successfully...");
+	std::vector<int> vec;
+	vec.push_back(1);
+	try
+	{
+		 int value = *easyfind(vec, 1);
+		 CHECK(value == 1);
+	}
+	catch (const std::exception& e)
+	{
+		CHECK("The element was find" == "The element wasn't find");
+	}
+}
+
+TEST_CASE("easyfind - invalid: search int on a int vtr")
+{
+	std::vector<int> vec;
+	vec.push_back(1);
+	try
+	{
+		 int value = *easyfind(vec, 0);
+		 CHECK(value == 1);
+	}
+	catch (const std::exception& e)
+	{
+		CHECK("The element wasn't find" == "The element wasn't find");
+	}
+}
+
+TEST_CASE("easyfind - valid: search int on a char vtr")
+{
+	std::vector<char> vec;
+	vec.push_back((int) 'X');
+	try
+	{
+		 int value = *easyfind(vec, (int) 'X');
+		 CHECK(value == (int) 'X');
+	}
+	catch (const std::exception& e)
+	{
+		CHECK("The element was find" == "The element wasn't find");
+	}
 }
