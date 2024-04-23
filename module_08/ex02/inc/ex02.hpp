8326caeb5d242c67951fc7ce8b078b1e053b23d1
Daniel Exposito lopez <dexposit@c2r4s6.42madrid.com>
2024-04-23 21:01:01 +0200

    testing exercise, look OK

--- module_08/ex02/inc/ex02.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef EX02_HPP
-#define EX02_HPP
-# include <iostream>
-typedef std::basic_string<char> str;
-
-#endif
--- module_08/ex02/src/main.cpp
+++ module_08/ex02/src/main.cpp
@@ -1,4 +1,3 @@
-#include <ex02.hpp>
 #include <MutantStack.hpp>
 
 void leaks(void)
@@ -7,27 +6,28 @@ void leaks(void)
 }
 int main(void)
 {
-	atexit(leaks);
-MutantStack<int> mstack;
-mstack.push(5);
-mstack.push(17);
-std::cout << mstack.top() << std::endl;
-mstack.pop();
-std::cout << mstack.size() << std::endl;
-mstack.push(3);
-mstack.push(5);
-mstack.push(737);
-//[...]
-mstack.push(0);
-MutantStack<int>::iterator it = mstack.begin();
-MutantStack<int>::iterator ite = mstack.end();
-++it;
---it;
-while (it != ite)
-{
-std::cout << *it << std::endl;
-++it;
-}
-std::stack<int> s(mstack);
-return 0;
+//	atexit(leaks);
+
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
