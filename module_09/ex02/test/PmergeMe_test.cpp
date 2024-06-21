a6fd9f1153201626cf9c10bf60222941aa7e3a99
Daniel Exposito lopez <dexposit@c1r10s6.42madrid.com>
2024-06-21 20:32:39 +0200

    adding short tests and function to generate random inputs

--- module_09/ex02/test/PmergeMe_test.cpp
+++ module_09/ex02/test/PmergeMe_test.cpp
@@ -6,24 +6,74 @@
 #include <exception>
 #include <stdexcept>
 #include <cstring>
+#define minNmb 1
+#define maxNmb 10000
+#define oddLength 3000
+#define evenLength 3001
 
-void test_invalid_sequence(const char* argv[]) {
-    PmergeMe obj(const_cast<char**>(argv));
+static std::string error = "Error: invalid positive integer \"";
+
+vector generateInput(int min, int max, bool repeat)
+{
+	vector input;
+	int length;
+
+	if (min == max)
+		length = min;
+	else
+		length = rand() % (max- min + 1) + min; 
+	if (repeat == true)
+		for (int i = 0; i < length; i++)
+			input.push_back(rand() % (maxNmb - minNmb + 1) + minNmb);
+	else
+		for (int i = 0; i < length; i++)
+		{
+			input.push_back(i);
+			std::random_shuffle(input.begin(), input.end());
+		}
+	return input;
+}
+
+
+void test_invalid_sequence(const char* argv[], std::string error) {
     try {
+    	PmergeMe obj(const_cast<char**>(argv));
         obj.shortContainersWithTimers();
         CHECK(false); // This should not be reached if an exception is thrown
     } catch (const std::exception& e) {
-        CHECK(strcmp(e.what(), "ERROR") == 0);
+        CHECK(std::string(e.what()) == error);
+        //CHECK(strcmp(e.what(), error) == 0);
     }
 }
+
 void test_valid_sequence(const char* argv[]) {                                   
-    PmergeMe obj(const_cast<char**>(argv));                                      
     try {                                                                        
+    	PmergeMe obj(const_cast<char**>(argv));                                      
         obj.shortContainersWithTimers();                                         
         const std::vector<unsigned int>& container = obj.getFirstContainer();
+        const std::list<unsigned int>& container2 = obj.getSecondContainer();
         
         // Check if the container is sorted
         bool is_sorted = std::is_sorted(container.begin(), container.end());
+        CHECK(is_sorted); // This should be true if the container is sorted
+        is_sorted = std::is_sorted(container2.begin(), container2.end());
+        CHECK(is_sorted); // This should be true if the container is sorted
+    } catch (const std::exception& e) {                                          
+        CHECK(false); // This should not be reached if an exception is not thrown
+    }                                                                            
+} 
+
+void test_valid_sequence(vector& argv) {                                   
+    try {                                                                        
+    	PmergeMe obj(argv);                                      
+        obj.shortContainersWithTimers();
+        const std::vector<unsigned int>& container = obj.getFirstContainer();
+        const std::list<unsigned int>& container2 = obj.getSecondContainer();
+        
+        // Check if the container is sorted
+        bool is_sorted = std::is_sorted(container.begin(), container.end());
+        CHECK(is_sorted); // This should be true if the container is sorted
+        is_sorted = std::is_sorted(container2.begin(), container2.end());
         CHECK(is_sorted); // This should be true if the container is sorted
     } catch (const std::exception& e) {                                          
         CHECK(false); // This should not be reached if an exception is not thrown
@@ -36,64 +86,84 @@ TEST_CASE("Short valid integer sequence")
     test_valid_sequence(argv);
 }
 
-
 TEST_CASE("Invalid - sequence with nmb > max_int") {
     const char* argv[] = {"999999999999999999999999999999", NULL};
-    test_invalid_sequence(argv);
+    test_invalid_sequence(argv, error + "999999999999999999999999999999\"");
 }
 
 TEST_CASE("Invalid - sequence with negative int") {
     const char* argv[] = {"-1", "2", "3", NULL};
-    test_invalid_sequence(argv);
+    test_invalid_sequence(argv, error + "-1\"");
 }
 
 TEST_CASE("Invalid - sequence with double value") {
     const char* argv[] = {"1.5", "2", "3", NULL};
-    test_invalid_sequence(argv);
+    test_invalid_sequence(argv, error + "1.5\"");
 }
 
 TEST_CASE("Invalid - sequence with float value") {
     const char* argv[] = {"1.0f", "2", "3", NULL};
-    test_invalid_sequence(argv);
+    test_invalid_sequence(argv, error + "1.0f\"");
 }
 
 TEST_CASE("Invalid - sequence with 'aaa98983'") {
     const char* argv[] = {"aaa98983", NULL};
-    test_invalid_sequence(argv);
+    test_invalid_sequence(argv, error + "aaa98983\"");
 }
 
 TEST_CASE("Invalid - sequence with '98983$·$&)'") {
     const char* argv[] = {"98983$·$&)", NULL};
-    test_invalid_sequence(argv);
+    test_invalid_sequence(argv, error + "98983$·$&)\"");
 }
 
 TEST_CASE("Invalid - sequence with 'dsjiowe'") {
     const char* argv[] = {"dsjiowe", NULL};
-    test_invalid_sequence(argv);
+    test_invalid_sequence(argv, error + "dsjiowe\"");
 }
 
 TEST_CASE("Invalid - sequence with 'A'") {
     const char* argv[] = {"A", NULL};
-    test_invalid_sequence(argv);
+    test_invalid_sequence(argv, error + "A\"");
 }
 
 TEST_CASE("Invalid - sequence with '-10'") {
     const char* argv[] = {"-10", NULL};
-    test_invalid_sequence(argv);
+    test_invalid_sequence(argv, error + "-10\"");
 }
 
 TEST_CASE("Invalid - sequence with '87e'") {
     const char* argv[] = {"87e", NULL};
-    test_invalid_sequence(argv);
+    test_invalid_sequence(argv, error + "87e\"");
 }
 
+TEST_CASE("Short - even sequence with no repeats")
+{
+	vector input = generateInput(evenLength, evenLength, false);
+	test_valid_sequence(input);
+}
+TEST_CASE("Short - odd sequence with no repeats")
+{
+	vector input = generateInput(oddLength, oddLength, false);
+	test_valid_sequence(input);
+}
+TEST_CASE("Short - even sequence with two ocurrences of the same integer")
+{
+	vector input = generateInput(evenLength, evenLength, true);
+	test_valid_sequence(input);
+}
+TEST_CASE("Short - odd sequence with only repeated characters")
+{
+	vector input = generateInput(oddLength, oddLength, true);
+	test_valid_sequence(input);
+}
 /* other usefull tests
-lengths 11 and 10
-lengths 2999 and 3000
-Short - even sequence with no repeats
-Short - odd sequence with no repeats
-Short - even sequence with two ocurrences of the same integer
-Short - odd sequence with only repeated characters
-Short - even sequence with two ocurrences of the same integer
-Short - odd sequence with only repeated characters
+TEST_CASE("Short - even sequence with two ocurrences of the same integer")
+{
+}
+TEST_CASE("Short - odd sequence with only repeated characters")
+{
+	vector input = generateInput(oddLength, oddLength, true);
+	test_valid_sequence(input);
+}
+
 */
