82e2bbecf317841eeb0f216610c5c17d79d5b8a5
Daniel Exposito lopez <dexposit@c3r9s6.42madrid.com>
2024-06-28 17:20:14 +0200

    delete comments

--- module_09/ex00/src/main.cpp
+++ module_09/ex00/src/main.cpp
@@ -10,28 +10,6 @@ void leaks(void)
 int main(int argc, char *argv[])
 {
 //	atexit(leaks);
-/*
-	std::cout << "The proyect was created sucessfully." << std::endl;
-	BitcoinExchange example;
-	std::cout << example << std::endl;
-//	example.printResults("input.txt");
-	BitcoinExchange testDate("dataSimple.csv", "simpleInput.txt");
-	std::cout << testDate << std::endl;
-	testDate.printResults("simpleInput.txt");
-*/
-/*
-	BitcoinExchange test = BitcoinExchange();
-	std::cout << "emptyInput:" << std::endl;
-	test.printResults("inputs/emptyInput.txt");
-	std::cout << "onlySpaces:" << std::endl;
-	test.printResults("inputs/onlySpaces.txt");
-	std::cout << "noContent:" << std::endl;
-	test.printResults("inputs/noContent.txt");
-	std::cout << "input:" << std::endl;
-	test.printResults("inputs/input.txt");
-	std::cout << "inputSubject:" << std::endl;
-	test.printResults("inputs/inputSubject.txt");
-*/
 	if (argc != 2)
 		return -1;
 	try
