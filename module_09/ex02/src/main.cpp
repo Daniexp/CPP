2398fe8d7919ad8ab302105e333bfc2b36c194f0
Daniel Exposito lopez <dexposit@c3r9s6.42madrid.com>
2024-06-28 19:18:14 +0200

    delete leaks

--- module_09/ex02/src/main.cpp
+++ module_09/ex02/src/main.cpp
@@ -1,14 +1,7 @@
 #include <PmergeMe.hpp>
 
-void leaks(void)
-{
-	system("leaks ex02");
-}
-
-
 int main(int argc, char* argv[])
 {
-	atexit(leaks);
 	if (argc <= 1)
 		return (-1); 
 	PmergeMe containers; 
