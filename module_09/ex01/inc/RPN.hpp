1e72fbd799ef4c2e6e216a891d2cb55b5859bbd3
Daniel Exposito lopez <dexposit@c1r6s1.42madrid.com>
2024-07-15 19:51:23 +0200

    delete old functionality methods

--- module_09/ex01/inc/RPN.hpp
+++ module_09/ex01/inc/RPN.hpp
@@ -4,14 +4,6 @@
 # include <stack>
 # include <string>
 
-typedef std::basic_string<char> str;
-
-std::stack<char>	newInvertPolishExpression(const std::string& src);
-long			calculatePolishExpression(std::stack<char>& expression);
-long			calculateExpression(long* nmb2, long *nmb1, char *op);
-bool			isNumber(char element);
-void			printStack(std::stack<char>& expression);
-//new Implementation with string against int
 double	calculateRPN(const std::string& src);
 double	calculateExpression(double nmb2, double nmb1, char op);
 #endif
